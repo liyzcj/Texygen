2ba49466160e170669d270f0bcdf6a8c14169ff1
liyzcj <liyz0912@gmail.com>
2019-05-12 10:43:55 +0800

    compute bleu for different samples

--- utils/others/ground_bleu.py
+++ utils/others/ground_bleu.py
@@ -7,24 +7,36 @@ sys.path.append('d:\\Texygen')
 from utils.metrics.Bleu import Bleu
 import os
 import json
+from time import time
 
 train_file = os.path.join('data', 'image_coco.txt')
 test_file = os.path.join('data', 'testdata', 'test_coco.txt')
 
 if __name__ == "__main__":
     
-  score = {}
+  samples = [900, 1000, 1100, 1200]
+  exp_num = 3
+  log_file = open('bleu_result.csv', 'a')
 
   bleu = Bleu(train_file, test_file)
 
-  for i in range(2,6):
-    bleu.gram = i
-    score[f"Bleu{i}"] = bleu.get_score(is_fast=False)
+  # head = "samples, Bleu-2, Bleu-3, Bleu-4, Bleu-5\n"
+  # log_file.write(head)
+  for nsam in samples:
+    bleu.sample_size = nsam
+    for n_exp in range(exp_num):
+      scores = [nsam]
+      for i in range(2,6):
+        print(f"Samples:{nsam}, Exp Number:{n_exp}, Gram: {i}")
+        bleu.gram = i
+        tic = time()
+        scores.append(bleu.get_score())
+        toc = time()
+        print(f"\t Time: {toc - tic:.1f}s")
+      buffer = ','.join([str(s) for s in scores])+'\n'
+      log_file.write(buffer)
+  log_file.close()
 
-  print(score)
-
-  with open('res.json', 'w') as f:
-    json.dump(score, f)
 
 ## Result
 # {
--- utils/others/ground_self_bleu.py
+++ utils/others/ground_self_bleu.py
@@ -5,6 +5,7 @@ Computing the self-bleu of training file.
 import sys
 sys.path.append('d:\\Texygen')
 from utils.metrics.SelfBleu import SelfBleu
+from time import time
 import os
 import json
 
@@ -14,14 +15,25 @@ test_file = os.path.join('data', 'testdata', 'test_coco.txt')
 if __name__ == "__main__":
     
   score = {}
+  samples = [1700, 1800, 1900, 2000]
+  exp_num = 3
+  log_file = open('selfbleu_result.csv', 'a')
 
   bleu = SelfBleu(train_file)
 
-  for i in range(2,6):
-    bleu.gram = i
-    score[f"Bleu{i}"] = bleu.get_score(is_fast=False)
-
-  print(score)
-
-  with open('res.json', 'w') as f:
-    json.dump(score, f)
+  # head = "samples, sBleu-2, sBleu-3, sBleu-4, sBleu-5\n"
+  # log_file.write(head)
+  for nsam in samples:
+    bleu.sample_size = nsam
+    for n_exp in range(exp_num):
+      scores = [nsam]
+      for i in range(2,6):
+        print(f"Samples:{nsam}, Exp Number:{n_exp}, Gram: {i}", end='')
+        bleu.gram = i
+        tic = time()
+        scores.append(bleu.get_score())
+        toc = time()
+        print(f"\t Time: {toc - tic:.1f}s")
+      buffer = ','.join([str(s) for s in scores])+'\n'
+      log_file.write(buffer)
+  log_file.close()
